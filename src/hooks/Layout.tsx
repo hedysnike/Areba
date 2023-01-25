04110dd7c6de8d19ffe0abafc1113eee03b231e6
Nikkolla <arebanikko@gmail.com>
2023-01-25 10:01:54 +0400

    assistant reapplied

--- src/hooks/Layout.tsx
+++ src/hooks/Layout.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from "react";
 import { Navigation } from "@/components/desktopcomponents/h&f/navigation";
 import Footer from "@/components/desktopcomponents/h&f/Footer";
+import { Assistant } from "@/components/desktopcomponents/h&f/assistant";
 
 export function Layout({ children }: PropsWithChildren) {
   return (
@@ -8,6 +9,7 @@ export function Layout({ children }: PropsWithChildren) {
       <Navigation />
       <main>{children}</main>
       <Footer />
+      <Assistant />
     </div>
   );
 }
---
+++ src/pages/news.tsx
@@ -0,0 +1,38 @@
+import { Layout } from "@/hooks/Layout";
+
+export default function News() {
+    return (
+      <div className="min-h-screen h-auto ">
+        <div className="text-center text-xl my-10">ჩვენი მეორე ფოსტი</div>
+        <div>
+          <div className="text-black w-5/6 mr-auto ml-auto">
+            Introducing the newest and most convenient way to get your car fixed!
+            <br />
+            <br />
+            Are you tired of spending hours at the car repair shop, trying to find the right mechanic for your needs? Do
+            you dread the thought of having to take your car to the shop, leaving it there for hours, and then having to
+            pick it up again?
+            <br /> <br />
+            Well, we have the solution for you! Introducing AREBA, the one-stop shop for all your car repair needs. Our
+            website makes it easy for you to request a specialist to take a look at your car, and then we'll take it to
+            the appropriate car mechanic for you. Once it's fixed, we'll bring it back to you - all for just 20Gel !
+            <br /> <br />
+            Not only do you save time and hassle by not having to go to the shop yourself, but you can also put up on our
+            website what parts of the car you want to sell and what parts you're looking to buy.
+            <br /> <br />
+            We are a marketplace that is geared towards making the process of buying and selling parts of the car easy and
+            fast.
+            <br /> <br />
+            Our team of experts is dedicated to providing you with the best service possible. We work with only the best
+            mechanics in the business, and we guarantee that your car will be in good hands.
+            <br /> <br />
+            So why waste your time and energy on car repairs? Let us take care of it for you! Visit our website at Areba
+            and request a specialist today. Your car will thank you!
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+
+News.Layout = Layout;
