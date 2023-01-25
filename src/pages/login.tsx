2d7d74b2e0da84b8d3b654f6c62ee1425892e03c
Nikkolla <arebanikko@gmail.com>
2023-01-25 07:31:16 +0400

    login page improved a bit

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,11 +4,11 @@ import { Layout } from "@/hooks/Layout";
 export default function Login() {
   return (
     <div className="min-h-[84vh] h-full">
-      <div className="w-3/4 mr-auto ml-auto h-full mt-5 border border-solid border-[#dcdcdcaf]">
-      {LLogin()}
-        <div className="grid grid-cols-1 h-full bg-white w-full">
-          <input className="w-11/12 mr-auto ml-auto  bg-gray-300 border border-solid border-gray-600 outline-none p-3  mt-5" type="text" />
-          <input className="w-11/12 mr-auto ml-auto  bg-gray-300 border border-solid border-gray-600 outline-none p-3  mt-5" type="text" />
+      <div className="w-3/4 mr-auto ml-auto h-[75vh] mt-5 border border-solid border-[#dcdcdcaf]">
+      <div className="h-auto"> {LLogin()}</div>
+        <div className="grid grid-cols-1 h-auto bg-white w-full">
+          <input className="mx-6  bg-white border border-solid border-[#C2C2C2] outline-none px-3 pb-1 pt-6 rounded text-sm mt-5" type="text" />
+          <input className="mx-6  bg-white border border-solid border-[#C2C2C2] outline-none px-3 pb-1 pt-6 rounded text-sm mt-5" type="text" />
         </div>
       </div>
     </div>
