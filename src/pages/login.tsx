d4963e66b5b23659c7708d54ed77adf374bf845d
Nikkolla <arebanikko@gmail.com>
2023-01-26 23:33:34 +0400

    floating placeholder further improved

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -11,20 +11,20 @@ export default function Login() {
           <div className="relative group m-4 mb-16">
             <input
               type="text"
-              className="pt-5 pb-2 pl-1 w-full font-normal peer text-sm leading-6 absolute outline-none border border-solid border-black placeholder-shown:z-0 z-20"
+              className="pt-5 pb-2 pl-1 w-full font-normal peer text-sm leading-6 absolute outline-none border border-solid  border-[#C2C2C2] focus:border-2 hover:border hover:delay-[100ms] focus:transition-all hover:transition-all ease-in focus:border-black hover:border-black placeholder-shown:z-0 z-20"
               placeholder=" "
             />
-            <label className="pl-1 pt-4 peer-focus:-translate-y-[11px] absolute pointer-events-none m-[1px]  transition-all delay-75 text-base peer-focus:text-xs  peer-focus:text-black">ელ ფოსტა *</label>
-            <label className="absolute peer-placeholder-shown:text-transparent z-20 text-black pl-1 pt-[5px] text-xs m-[1px] ">ელ ფოსტა *</label>
+            <label className="pl-1 pt-4 peer-focus:-translate-y-[11px] absolute pointer-events-none m-[1px]  transition-all text-base peer-focus:text-xs text-[#9b9b9b]  peer-focus:text-[#9b9b9b]">ელ ფოსტა *</label>
+            <label className="absolute peer-placeholder-shown:text-transparent z-20 text-[#9b9b9b] pl-1 pt-[5px] text-xs m-[1px] ">ელ ფოსტა *</label>
           </div>
           <div className="relative group m-4 mb-16">
             <input
               type="text"
-              className="pt-5 pb-2 pl-1 w-full font-normal peer text-sm leading-6 absolute outline-none border border-solid border-black placeholder-shown:z-0 z-20"
+              className="pt-5 pb-2 pl-1 w-full font-normal peer text-sm leading-6 absolute outline-none border border-solid  border-[#C2C2C2] focus:border-2 hover:border hover:delay-[100ms] focus:transition-all hover:transition-all ease-in focus:border-black hover:border-black placeholder-shown:z-0 z-20"
               placeholder=" "
             />
-            <label className="pl-1 pt-4 peer-focus:-translate-y-[11px] absolute pointer-events-none m-[1px]  transition-all delay-75 text-base peer-focus:text-xs  peer-focus:text-black">პაროლი *</label>
-            <label className="absolute peer-placeholder-shown:text-transparent z-20 text-black pl-1 pt-[5px] text-xs m-[1px] ">პაროლი *</label>
+            <label className="pl-1 pt-4 peer-focus:-translate-y-[11px] absolute pointer-events-none m-[1px]  transition-all text-base peer-focus:text-xs text-[#9b9b9b]  peer-focus:text-[#9b9b9b]">პაროლი *</label>
+            <label className="absolute peer-placeholder-shown:text-transparent z-20 text-[#9b9b9b] pl-1 pt-[5px] text-xs m-[1px] ">პაროლი ფოსტა *</label>
           </div>
           <div className="mt-20"></div>
         </div>
