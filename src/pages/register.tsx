33d86270ea0feb63baa598ec765cf33793079f81
Nikkolla <arebanikko@gmail.com>
2023-01-27 04:02:05 +0400

    registration page updated

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -2,69 +2,75 @@ import { LLogin } from "@/components/desktopcomponents/registertoggle";
 import { Layout } from "@/hooks/Layout";
 
 export default function Register() {
-    return (
-      <div className="min-h-[84vh] h-full">
+  return (
+    <div className="min-h-[84vh] h-full">
       <div className="w-3/4 mr-auto ml-auto h-[75vh] mt-5 border border-solid border-[#dcdcdcaf]">
-      <div className="h-auto"> {LLogin()}</div>
+        <div className="h-auto"> {LLogin()}</div>
         <div className="grid grid-cols-1 h-auto bg-white w-full">
-        <div className="relative group m-4 mb-16">
+          <div className="relative rounded-[4px] group m-4 pb-16">
             <input
               type="text"
-              className="pt-5 pb-2 pl-1 w-full font-normal peer text-sm leading-6 absolute outline-none border border-solid  border-[#C2C2C2] focus:border-2 hover:border hover:delay-[100ms] focus:transition-all hover:transition-all ease-in focus:border-black hover:border-black placeholder-shown:z-0 z-20"
+              className="pt-5 rounded-md pb-3 pl-2 w-full font-normal peer text-sm leading-6 absolute outline-none ring-1 ring-solid  ring-[#C2C2C2] hover:ring-1 focus:ring-2 hover:delay-[100ms] hover:transition-all ease-in focus:ring-black hover:ring-black placeholder-shown:z-0 z-20"
               placeholder=" "
             />
-            <label className="pl-1 pt-4 peer-focus:-translate-y-[11px] absolute pointer-events-none m-[1px]  transition-all text-base peer-focus:text-xs text-[#9b9b9b]  peer-focus:text-[#9b9b9b]">ქვეყანა *</label>
-            <label className="absolute peer-placeholder-shown:text-transparent pointer-events-none z-20 text-[#9b9b9b] pl-1 pt-[5px] text-xs m-[1px] ">ქვეყანა *</label>
+            <label className="pl-2 pt-3 text-xs -translate-y-[10px] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-base peer-focus:-translate-y-[10px] m-[1px] transition-all peer-focus:text-xs text-[#9b9b9b] peer-focus:text-[#9b9b9b] absolute pointer-events-none z-20">
+              ქვეყანა *
+            </label>
           </div>
-          <div className="relative group m-4 mb-16">
+          <div className="relative rounded-[4px] group m-4 pb-16">
             <input
               type="text"
-              className="pt-5 pb-2 pl-1 w-full font-normal peer text-sm leading-6 absolute outline-none border border-solid  border-[#C2C2C2] focus:border-2 hover:border hover:delay-[100ms] focus:transition-all hover:transition-all ease-in focus:border-black hover:border-black placeholder-shown:z-0 z-20"
+              className="pt-5 rounded-md pb-3 pl-2 w-full font-normal peer text-sm leading-6 absolute outline-none ring-1 ring-solid  ring-[#C2C2C2] hover:ring-1 focus:ring-2 hover:delay-[100ms] hover:transition-all ease-in focus:ring-black hover:ring-black placeholder-shown:z-0 z-20"
               placeholder=" "
             />
-            <label className="pl-1 pt-4 peer-focus:-translate-y-[11px] absolute pointer-events-none m-[1px]  transition-all text-base peer-focus:text-xs text-[#9b9b9b]  peer-focus:text-[#9b9b9b]">ქალაქი *</label>
-            <label className="absolute peer-placeholder-shown:text-transparent pointer-events-none z-20 text-[#9b9b9b] pl-1 pt-[5px] text-xs m-[1px] ">ქალაქი *</label>
+            <label className="pl-2 pt-3 text-xs -translate-y-[10px] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-base peer-focus:-translate-y-[10px] m-[1px] transition-all peer-focus:text-xs text-[#9b9b9b] peer-focus:text-[#9b9b9b] absolute pointer-events-none z-20">
+              ქალაქი *
+            </label>
           </div>
-          <div className="relative group m-4 mb-16">
+          <div className="relative rounded-[4px] group m-4 pb-16">
             <input
               type="text"
-              className="pt-5 pb-2 pl-1 w-full font-normal peer text-sm leading-6 absolute outline-none border border-solid  border-[#C2C2C2] focus:border-2 hover:border hover:delay-[100ms] focus:transition-all hover:transition-all ease-in focus:border-black hover:border-black placeholder-shown:z-0 z-20"
+              className="pt-5 rounded-md pb-3 pl-2 w-full font-normal peer text-sm leading-6 absolute outline-none ring-1 ring-solid  ring-[#C2C2C2] hover:ring-1 focus:ring-2 hover:delay-[100ms] hover:transition-all ease-in focus:ring-black hover:ring-black placeholder-shown:z-0 z-20"
               placeholder=" "
             />
-            <label className="pl-1 pt-4 peer-focus:-translate-y-[11px] absolute pointer-events-none m-[1px]  transition-all text-base peer-focus:text-xs text-[#9b9b9b]  peer-focus:text-[#9b9b9b]">სახელი *</label>
-            <label className="absolute peer-placeholder-shown:text-transparent pointer-events-none z-20 text-[#9b9b9b] pl-1 pt-[5px] text-xs m-[1px] ">სახელი *</label>
+            <label className="pl-2 pt-3 text-xs -translate-y-[10px] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-base peer-focus:-translate-y-[10px] m-[1px] transition-all peer-focus:text-xs text-[#9b9b9b] peer-focus:text-[#9b9b9b] absolute pointer-events-none z-20">
+              სახელი *
+            </label>
           </div>
-          <div className="relative group m-4 mb-16">
+          <div className="relative rounded-[4px] group m-4 pb-16">
             <input
               type="text"
-              className="pt-5 pb-2 pl-1 w-full font-normal peer text-sm leading-6 absolute outline-none border border-solid  border-[#C2C2C2] focus:border-2 hover:border hover:delay-[100ms] focus:transition-all hover:transition-all ease-in focus:border-black hover:border-black placeholder-shown:z-0 z-20"
+              className="pt-5 rounded-md pb-3 pl-2 w-full font-normal peer text-sm leading-6 absolute outline-none ring-1 ring-solid  ring-[#C2C2C2] hover:ring-1 focus:ring-2 hover:delay-[100ms] hover:transition-all ease-in focus:ring-black hover:ring-black placeholder-shown:z-0 z-20"
               placeholder=" "
             />
-            <label className="pl-1 pt-4 peer-focus:-translate-y-[11px] absolute pointer-events-none m-[1px]  transition-all text-base peer-focus:text-xs text-[#9b9b9b]  peer-focus:text-[#9b9b9b]">გვარი *</label>
-            <label className="absolute peer-placeholder-shown:text-transparent pointer-events-none z-20 text-[#9b9b9b] pl-1 pt-[5px] text-xs m-[1px] ">გვარი *</label>
+            <label className="pl-2 pt-3 text-xs -translate-y-[10px] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-base peer-focus:-translate-y-[10px] m-[1px] transition-all peer-focus:text-xs text-[#9b9b9b] peer-focus:text-[#9b9b9b] absolute pointer-events-none z-20">
+              გვარი *
+            </label>
           </div>
-          <div className="relative group m-4 mb-16">
+          <div className="relative rounded-[4px] group m-4 pb-16">
             <input
               type="text"
-              className="pt-5 pb-2 pl-1 w-full font-normal peer text-sm leading-6 absolute outline-none border border-solid  border-[#C2C2C2] focus:border-2 hover:border hover:delay-[100ms] focus:transition-all hover:transition-all ease-in focus:border-black hover:border-black placeholder-shown:z-0 z-20"
+              className="pt-5 rounded-md pb-3 pl-2 w-full font-normal peer text-sm leading-6 absolute outline-none ring-1 ring-solid  ring-[#C2C2C2] hover:ring-1 focus:ring-2 hover:delay-[100ms] hover:transition-all ease-in focus:ring-black hover:ring-black placeholder-shown:z-0 z-20"
               placeholder=" "
             />
-            <label className="pl-1 pt-4 peer-focus:-translate-y-[11px] absolute pointer-events-none m-[1px]  transition-all text-base peer-focus:text-xs text-[#9b9b9b]  peer-focus:text-[#9b9b9b]">მობილური *</label>
-            <label className="absolute peer-placeholder-shown:text-transparent pointer-events-none z-20 text-[#9b9b9b] pl-1 pt-[5px] text-xs m-[1px] ">მობილური *</label>
+            <label className="pl-2 pt-3 text-xs -translate-y-[10px] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-base peer-focus:-translate-y-[10px] m-[1px] transition-all peer-focus:text-xs text-[#9b9b9b] peer-focus:text-[#9b9b9b] absolute pointer-events-none z-20">
+              მობილური *
+            </label>
           </div>
-          <div className="relative group m-4 mb-16">
+          <div className="relative rounded-[4px] group m-4 pb-16">
             <input
               type="text"
-              className="pt-5 pb-2 pl-1 w-full font-normal peer text-sm leading-6 absolute outline-none border border-solid  border-[#C2C2C2] focus:border-2 hover:border hover:delay-[100ms] focus:transition-all hover:transition-all ease-in focus:border-black hover:border-black placeholder-shown:z-0 z-20"
+              className="pt-5 rounded-md pb-3 pl-2 w-full font-normal peer text-sm leading-6 absolute outline-none ring-1 ring-solid  ring-[#C2C2C2] hover:ring-1 focus:ring-2 hover:delay-[100ms] hover:transition-all ease-in focus:ring-black hover:ring-black placeholder-shown:z-0 z-20"
               placeholder=" "
             />
-            <label className="pl-1 pt-4 peer-focus:-translate-y-[11px] absolute pointer-events-none m-[1px]  transition-all text-base peer-focus:text-xs text-[#9b9b9b]  peer-focus:text-[#9b9b9b]">პაროლი *</label>
-            <label className="absolute peer-placeholder-shown:text-transparent pointer-events-none z-20 text-[#9b9b9b] pl-1 pt-[5px] text-xs m-[1px] ">პაროლი *</label>
-          </div>
+            <label className="pl-2 pt-3 text-xs -translate-y-[10px] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-base peer-focus:-translate-y-[10px] m-[1px] transition-all peer-focus:text-xs text-[#9b9b9b] peer-focus:text-[#9b9b9b] absolute pointer-events-none z-20">
+              პაროლი *
+            </label>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 Register.Layout = Layout;
