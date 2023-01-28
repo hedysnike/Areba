f804f7c085ebfb99290e510667ab73cafcb395ad
Nikkolla <arebanikko@gmail.com>
2023-01-28 23:35:15 +0400

    selector used succesfully

--- src/components/desktopcomponents/h&f/specialistmodal.tsx
+++ src/components/desktopcomponents/h&f/specialistmodal.tsx
@@ -2,12 +2,23 @@ import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
 import { Stepper } from "@mantine/core";
 import { Icon } from "@iconify/react";
+import { Select } from "@mui/material";
+import { Combobox } from "@headlessui/react";
+import { Makes } from "@/assets/makes";
 
 interface ModalProps {
   open: boolean;
   onClose: () => void;
   children: React.ReactNode;
 }
+
+const people = [
+  { id: 1, name: "Durward Reynolds" },
+  { id: 2, name: "Kenton Towne" },
+  { id: 3, name: "Therese Wunsch" },
+  { id: 4, name: "Benedict Kessler" },
+  { id: 5, name: "Katelyn Rohan" },
+];
 
 function Modal({ open, onClose, children }: ModalProps) {
   return (
@@ -25,7 +36,7 @@ function Modal({ open, onClose, children }: ModalProps) {
           <div className="fixed inset-0 bg-black bg-opacity-25" />
         </Transition.Child>
         <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <div className="flex items-center justify-center min-h-full p-4 text-center">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -35,7 +46,7 @@ function Modal({ open, onClose, children }: ModalProps) {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full md:max-w-2xl max-w-md shadow-xl  transform overflow-hidden bg-white text-left text-white align-middle rounded-md transition-all">
+              <Dialog.Panel className="w-full max-w-md overflow-hidden text-left text-white align-middle transition-all transform bg-white rounded-md shadow-xl md:max-w-2xl">
                 {children}
               </Dialog.Panel>
             </Transition.Child>
@@ -48,12 +59,30 @@ function Modal({ open, onClose, children }: ModalProps) {
 
 export function SpecialistModal({ openModal, onClose }: { openModal: boolean; onClose: () => void }) {
   const [active, setActive] = useState(1);
+  const [selectedPerson, setSelectedPerson] = useState(people[0]);
+  const [query, setQuery] = useState("");
+  const [selected, setSelected] = useState(Makes[0]);
+
+  const filteredMakes =
+    query === ""
+      ? Makes
+      : Makes.filter((make) =>
+          make.title.toLowerCase().replace(/\s+/g, "").includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
   const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
   const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
 
+  const filteredPeople =
+    query === ""
+      ? people
+      : people.filter((person) => {
+          return person.name.toLowerCase().includes(query.toLowerCase());
+        });
+
   return (
     <Modal open={openModal} onClose={onClose}>
-      <div className="text-black flex flex-col items-center justify-center w-full z-30">
+      <div className="z-30 flex flex-col items-center justify-center w-full text-black">
         <div className="w-full mt-3 ">
           <Stepper
             iconSize={24}
@@ -65,7 +94,7 @@ export function SpecialistModal({ openModal, onClose }: { openModal: boolean; on
             onStepClick={setActive}
             breakpoint="sm"
           >
-            <Stepper.Step className="pl-5 py-3">
+            <Stepper.Step className="py-3 pl-5">
               <h1 className="bg-[#FF7600] w-full text-center text-white py-3 text-sm outline-none">
                 სპეციალისტის გამოძახება - 20 ლარი
               </h1>
@@ -91,7 +120,7 @@ export function SpecialistModal({ openModal, onClose }: { openModal: boolean; on
                     ჩაწერეთ სად მოვიდეს სპეციალისტი
                   </label>
                 </div>
-                <div className="w-full flex ">
+                <div className="flex w-full ">
                   <div className="relative rounded-[4px] group m-4 mr-0 pb-12 w-[60%]">
                     <input
                       type="text"
@@ -113,7 +142,7 @@ export function SpecialistModal({ openModal, onClose }: { openModal: boolean; on
                     </label>
                   </div>{" "}
                 </div>
-                <div className="justify-start px-4 mb-7 w-full">
+                <div className="justify-start w-full px-4 mb-7">
                   <button className="bg-[#FF7600] text-white p-2 rounded mt-7 flex" onClick={nextStep}>
                     <h1>გაგრძელება</h1>
                   </button>
@@ -125,119 +154,110 @@ export function SpecialistModal({ openModal, onClose }: { openModal: boolean; on
                 <h1 className="bg-[#FF7600] w-full text-center text-white py-3 text-sm">
                   სპეციალისტის გამოძახება - 20 ლარი
                 </h1>
-                <div className="flex flex-col w-full">
-                  <div className="relative rounded-[4px] group m-4 pb-11">
-                    <input
-                      type="text"
-                      className="pt-5 rounded-md pb-3 pl-2 w-full font-normal peer text-sm leading-6 absolute outline-none ring-1 ring-solid  ring-[#C2C2C2] hover:ring-1 focus:ring-2 hover:delay-[100ms] hover:transition-all ease-in focus:ring-black hover:ring-black placeholder-shown:z-0 z-20"
-                      placeholder=" "
-                    />
-                    <label className="pl-2 pt-3 text-xs -translate-y-[10px] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-base peer-focus:-translate-y-[10px] m-[1px] transition-all peer-focus:text-xs text-[#9b9b9b] peer-focus:text-[#9b9b9b] absolute pointer-events-none z-20">
-                      ჩაწერეთ რისი გაკეთება გსურთ მანქანაზე?
-                    </label>
-                  </div>
-                  <div className="relative rounded-[4px] group m-4 pb-11">
-                    <input
-                      type="text"
-                      className="pt-5 rounded-md pb-3 pl-2 w-full font-normal peer text-sm leading-6 absolute outline-none ring-1 ring-solid  ring-[#C2C2C2] hover:ring-1 focus:ring-2 hover:delay-[100ms] hover:transition-all ease-in focus:ring-black hover:ring-black placeholder-shown:z-0 z-20"
-                      placeholder=" "
-                    />
-                    <label className="pl-2 pt-3 text-xs -translate-y-[10px] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-base peer-focus:-translate-y-[10px] m-[1px] transition-all peer-focus:text-xs text-[#9b9b9b] peer-focus:text-[#9b9b9b] absolute pointer-events-none z-20">
-                      ჩაწერეთ სად მოვიდეს სპეციალისტი
-                    </label>
-                  </div>
-                  <div className="w-full flex ">
-                    <div className="relative rounded-[4px] group m-4 mr-0 pb-12 w-[60%]">
-                      <input
-                        type="text"
-                        className="pt-5 rounded-md pb-3 pl-2 w-full font-normal peer text-sm leading-6 absolute outline-none ring-1 ring-solid  ring-[#C2C2C2] hover:ring-1 focus:ring-2 hover:delay-[100ms] hover:transition-all ease-in focus:ring-black hover:ring-black placeholder-shown:z-0 z-20"
-                        placeholder=" "
-                      />
-                      <label className="pl-2 pt-3 text-xs -translate-y-[10px] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-base peer-focus:-translate-y-[10px] m-[1px] transition-all peer-focus:text-xs text-[#9b9b9b] peer-focus:text-[#9b9b9b] absolute pointer-events-none z-20">
-                        წელი/თვე/რიცხვი
-                      </label>
+                <div className="m-4">
+                  <div className="flex flex-col w-full">
+                    <div className="relative rounded-[4px] group m-4 pb-11 border border-solid border-black">
+                      <Combobox value={selectedPerson} onChange={setSelectedPerson}>
+                        <Combobox.Input className="pt-5 pb-3 pl-2" onChange={(event) => setQuery(event.target.value)} />
+                        <Combobox.Options>
+                          {filteredPeople.map((person) => (
+                            <Combobox.Option key={person.id} value={person} className="">
+                              <div className="py-1 text-black">{person.name}</div>
+                            </Combobox.Option>
+                          ))}
+                        </Combobox.Options>
+                      </Combobox>
                     </div>
-                    <div className="relative rounded-[4px] group m-4 ml-2  pb-12 w-[40%]">
-                      <input
-                        type="text"
-                        className="pt-5 rounded-md pb-3 pl-2 w-full font-normal peer text-sm leading-6 absolute outline-none ring-1 ring-solid  ring-[#C2C2C2] hover:ring-1 focus:ring-2 hover:delay-[100ms] hover:transition-all ease-in focus:ring-black hover:ring-black placeholder-shown:z-0 z-20"
-                        placeholder=" "
-                      />
-                      <label className="pl-2 pt-3 text-xs -translate-y-[10px] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-base peer-focus:-translate-y-[10px] m-[1px] transition-all peer-focus:text-xs text-[#9b9b9b] peer-focus:text-[#9b9b9b] absolute pointer-events-none z-20">
-                        საათი:წუთი
-                      </label>
-                    </div>{" "}
                   </div>
-                  <div className="justify-start px-4 mb-7 w-full flex">
-                  <button className="bg-[#FF7600] text-white p-2 rounded mt-7 flex text-sm" onClick={nextStep}>
-                  <h1>გაგრძელება</h1>
-                </button>
-                <button className="p-2 ml-2 mt-7 text-[#FF7600] text-sm" onClick={prevStep}>
-                  <h1>უკან</h1>
-                </button>
+                </div>
+                <div className="flex flex-col w-full">
+                  <div className="flex justify-start w-full px-4 mb-7">
+                    <button className="bg-[#FF7600] text-white p-2 rounded mt-7 flex text-sm" onClick={nextStep}>
+                      <h1>გაგრძელება</h1>
+                    </button>
+                    <button className="p-2 ml-2 mt-7 text-[#FF7600] text-sm" onClick={prevStep}>
+                      <h1>უკან</h1>
+                    </button>
                   </div>
                 </div>
               </div>
             </Stepper.Step>
-            <Stepper.Step className="pr-5 py-3">
-            <div>
-                <h1 className="bg-[#FF7600] w-full text-center text-white py-3 text-sm">
+            <Stepper.Step className="py-3 pr-5">
+              <div>
+                <h1 className="bg-[#FF7600] w-full text-center text-white py-3 text-sm mb-">
                   სპეციალისტის გამოძახება - 20 ლარი
                 </h1>
-                <div className="flex flex-col w-full">
-                  <div className="relative rounded-[4px] group m-4 pb-11">
-                    <input
-                      type="text"
-                      className="pt-5 rounded-md pb-3 pl-2 w-full font-normal peer text-sm leading-6 absolute outline-none ring-1 ring-solid  ring-[#C2C2C2] hover:ring-1 focus:ring-2 hover:delay-[100ms] hover:transition-all ease-in focus:ring-black hover:ring-black placeholder-shown:z-0 z-20"
-                      placeholder=" "
-                    />
-                    <label className="pl-2 pt-3 text-xs -translate-y-[10px] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-base peer-focus:-translate-y-[10px] m-[1px] transition-all peer-focus:text-xs text-[#9b9b9b] peer-focus:text-[#9b9b9b] absolute pointer-events-none z-20">
-                      ჩაწერეთ რისი გაკეთება გსურთ მანქანაზე?
-                    </label>
-                  </div>
-                  <div className="relative rounded-[4px] group m-4 pb-11">
-                    <input
-                      type="text"
-                      className="pt-5 rounded-md pb-3 pl-2 w-full font-normal peer text-sm leading-6 absolute outline-none ring-1 ring-solid  ring-[#C2C2C2] hover:ring-1 focus:ring-2 hover:delay-[100ms] hover:transition-all ease-in focus:ring-black hover:ring-black placeholder-shown:z-0 z-20"
-                      placeholder=" "
-                    />
-                    <label className="pl-2 pt-3 text-xs -translate-y-[10px] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-base peer-focus:-translate-y-[10px] m-[1px] transition-all peer-focus:text-xs text-[#9b9b9b] peer-focus:text-[#9b9b9b] absolute pointer-events-none z-20">
-                      ჩაწერეთ სად მოვიდეს სპეციალისტი
-                    </label>
-                  </div>
-                  <div className="w-full flex ">
-                    <div className="relative rounded-[4px] group m-4 mr-0 pb-12 w-[60%]">
-                      <input
-                        type="text"
-                        className="pt-5 rounded-md pb-3 pl-2 w-full font-normal peer text-sm leading-6 absolute outline-none ring-1 ring-solid  ring-[#C2C2C2] hover:ring-1 focus:ring-2 hover:delay-[100ms] hover:transition-all ease-in focus:ring-black hover:ring-black placeholder-shown:z-0 z-20"
-                        placeholder=" "
-                      />
-                      <label className="pl-2 pt-3 text-xs -translate-y-[10px] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-base peer-focus:-translate-y-[10px] m-[1px] transition-all peer-focus:text-xs text-[#9b9b9b] peer-focus:text-[#9b9b9b] absolute pointer-events-none z-20">
-                        წელი/თვე/რიცხვი
-                      </label>
+                <div className="flex flex-col w-full px-4">
+                  <Combobox value={selected} onChange={setSelected}>
+                    <div className="relative mt-1">
+                      <div className="relative w-full overflow-hidden text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+                        <Combobox.Input
+                          className="w-full py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 border border-black focus:ring-0"
+                          displayValue={(make) => make.title}
+                          onChange={(event) => setQuery(event.target.value)}
+                        />
+                        <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                          <Icon icon="material-symbols:10k" color="black" width="20" height="20" aria-hidden="true" />
+                        </Combobox.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                        afterLeave={() => setQuery("")}
+                      >
+                        <Combobox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                          {filteredMakes.length === 0 && query !== "" ? (
+                            <div className="relative px-4 py-2 text-gray-700 cursor-default select-none">
+                              Nothing found.
+                            </div>
+                          ) : (
+                            filteredMakes.map((make) => (
+                              <Combobox.Option
+                                key={make.id}
+                                className={({ active }) =>
+                                  `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                    active ? "bg-teal-600 text-white" : "text-gray-900"
+                                  }`
+                                }
+                                value={make}
+                              >
+                                {({ selected, active }) => (
+                                  <>
+                                    <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
+                                      {make.title}
+                                    </span>
+                                    {selected ? (
+                                      <span
+                                        className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                          active ? "text-white" : "text-teal-600"
+                                        }`}
+                                      >
+                                      </span>
+                                    ) : null}
+                                  </>
+                                )}
+                              </Combobox.Option>
+                            ))
+                          )}
+                        </Combobox.Options>
+                      </Transition>
                     </div>
-                    <div className="relative rounded-[4px] group m-4 ml-2  pb-12 w-[40%]">
-                      <input
-                        type="text"
-                        className="pt-5 rounded-md pb-3 pl-2 w-full font-normal peer text-sm leading-6 absolute outline-none ring-1 ring-solid  ring-[#C2C2C2] hover:ring-1 focus:ring-2 hover:delay-[100ms] hover:transition-all ease-in focus:ring-black hover:ring-black placeholder-shown:z-0 z-20"
-                        placeholder=" "
-                      />
-                      <label className="pl-2 pt-3 text-xs -translate-y-[10px] peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-base peer-focus:-translate-y-[10px] m-[1px] transition-all peer-focus:text-xs text-[#9b9b9b] peer-focus:text-[#9b9b9b] absolute pointer-events-none z-20">
-                        საათი:წუთი
-                      </label>
-                    </div>{" "}
-                  </div>
-                  <div className="justify-start px-4 mb-7 w-full flex">
-                  <button className="bg-[#FF7600] text-white p-2 rounded mt-7 flex text-sm" onClick={nextStep}>
-                  <h1>გაგრძელება</h1>
-                </button>
-                <button className="p-2 ml-2 mt-7 text-[#FF7600] text-sm" onClick={prevStep}>
-                  <h1>უკან</h1>
-                </button>
+                  </Combobox>
+
+                  <div className="flex w-full "></div>
+                  <div className="flex justify-start w-full px-4 mb-7">
+                    <button className="bg-[#FF7600] text-white p-2 rounded mt-7 flex text-sm" onClick={nextStep}>
+                      <h1>გაგრძელება</h1>
+                    </button>
+                    <button className="p-2 ml-2 mt-7 text-[#FF7600] text-sm" onClick={prevStep}>
+                      <h1>უკან</h1>
+                    </button>
                   </div>
                 </div>
               </div>
-              </Stepper.Step>
+            </Stepper.Step>
             <Stepper.Completed>Completed, click back button to get to previous step</Stepper.Completed>
           </Stepper>
           <div className="flex items-center justify-center m-2"></div>
