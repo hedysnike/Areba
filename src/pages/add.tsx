d535a7576ec8f8a4e8fd9f0988ebb9e43740af4e
Nikkolla <arebanikko@gmail.com>
2023-02-02 19:28:28 +0400

    typescript errors fixed

--- src/pages/add.tsx
+++ src/pages/add.tsx
@@ -1,11 +1,26 @@
 import { Makes } from "@/assets/makes";
 import { Models } from "@/assets/models";
 import { Layoutlogged } from "@/hooks/Layoutlogged";
+import { ThemeProvider } from "@emotion/react";
 import { Autocomplete, Chip, createTheme, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import { useState } from "react";
 
-const completetheme = createTheme({
+const theme = createTheme({
   components: {
+    MuiSelect: {
+      variants: [
+        {
+          props: { variant: "filled" },
+          style: {
+            border: "1px solid #E0E0E0",
+            backgroundColor: "transparent",
+            "&:hover": {
+              backgroundColor: "transparent",
+            },
+          },
+        },
+      ],
+    },
     MuiTextField: {
       styleOverrides: {
         root: {
@@ -41,32 +56,15 @@ const completetheme = createTheme({
   },
 });
 
-const theme = createTheme({
-  components: {
-    MuiSelect: {
-      variants: [
-        {
-          props: { variant: "filled" },
-          style: {
-            border: "1px solid #E0E0E0",
-            backgroundColor: "transparent",
-            "&:hover": {
-              backgroundColor: "transparent",
-            },
-          },
-        },
-      ],
-    },
-  },
-});
 
 export default function Add() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="w-3/4 max-w-6xl min-h-[75vh] h-auto ml-auto mr-auto">
       <h1 className="flex flex-col items-center my-[52px]">
         <FormControl variant="filled" sx={{ width: "95%", marginRight: "auto", marginLeft: "auto" }}>
           <InputLabel>ნაწილის მდგომარეობა</InputLabel>
-          <Select theme={theme} variant="filled">
+          <Select variant="filled">
             <MenuItem value={5}>ახალი</MenuItem>
             <MenuItem value={4}>მეორადი</MenuItem>
             <MenuItem value={3}>ახალიც და მეორადიც</MenuItem>
@@ -77,10 +75,10 @@ export default function Add() {
             id="tags-filled"
             sx={{ width: "95%", marginRight: "auto", marginLeft: "auto" }}
             options={Makes.map((make) => make.title)}
-            theme={completetheme}
             renderTags={(value, getTagProps) =>
               value.map((make, index) => (
-                <Chip variant="outlined" size="small" key={make.id} {...getTagProps({ index })} />
+                // eslint-disable-next-line react/jsx-key
+                <Chip variant="outlined" size="small" {...getTagProps({ index })} />
               ))
             }
             renderInput={(params) => (
@@ -98,10 +96,10 @@ export default function Add() {
             id="tags-filled"
             sx={{ width: "95%", marginRight: "auto", marginLeft: "auto" }}
             options={Models.map((model) => model.title)}
-            theme={completetheme}
             renderTags={(value, getTagProps) =>
               value.map((model, index) => (
-                <Chip variant="outlined" size="small" key={model.id} {...getTagProps({ index })} />
+                // eslint-disable-next-line react/jsx-key
+                <Chip variant="outlined" size="small" {...getTagProps({ index })} />
               ))
             }
             renderInput={(params) => (
@@ -117,7 +115,7 @@ export default function Add() {
         <div className="w-[95%] mr-auto ml-auto mt-5">
           <FormControl variant="filled" sx={{ width: "100%" }}>
             <InputLabel>აირჩიეთ წელი</InputLabel>
-            <Select theme={theme} variant="filled">
+            <Select variant="filled">
               <MenuItem value={2}>მეორადი</MenuItem>
               <MenuItem value={1}>ახალიც და მეორადიც</MenuItem>
             </Select>
@@ -126,7 +124,6 @@ export default function Add() {
 
         <div className="w-[95%] mt-5">
           <TextField
-            theme={completetheme}
             className="w-[100%]"
             id="outlined-multiline-static"
             label="დეტალური ინფორმაცია"
@@ -138,7 +135,6 @@ export default function Add() {
         </div>
         <div className="w-[95%] mt-5">
           <TextField
-            theme={completetheme}
             className="w-[100%]"
             id="outlined-multiline-static"
             label="VIN კოდი"
@@ -148,6 +144,7 @@ export default function Add() {
         <button className="bg-[#FF7600] px-4 py-2 flex justify-start text-white mt-5 rounded text-sm">დამატება</button>
       </h1>
     </div>
+    </ThemeProvider>
   );
 }
 
