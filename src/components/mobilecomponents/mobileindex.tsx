dd4beed304b96ed47f7ae7b5e0aa655078a1d4bf
Nika <arebanikko@gmail.com>
2023-05-31 08:29:55 +0400

    error removed

--- src/components/mobilecomponents/mobileindex.tsx
+++ src/components/mobilecomponents/mobileindex.tsx
@@ -1,6 +1,5 @@
 import { Icon } from "@iconify/react";
 import { useState } from "react";
-import { MobileLayout } from "@/hooks/MLayout";
 import { SpecialistModal } from "../desktopcomponents/specialistmodal/specialistmodal";
 import Link from "next/link";
 
@@ -22,38 +21,38 @@ export function MobileIndex() {
           <h1>სპეციალისტის გამოძახება</h1>
         </button>
       </div>
-      <h1 className="text-center text-3xl font-medium">ჩვენ გთავაზობთ</h1>
-      <div className=" w-5/6 ml-auto mr-auto text-center">
+      <h1 className="text-3xl font-medium text-center">ჩვენ გთავაზობთ</h1>
+      <div className="w-5/6 ml-auto mr-auto text-center ">
         <div className="flex flex-col items-center justify-center">
           <img src="https://i.imgur.com/Y8Di5eV.png" alt="Service Image" />
           <span>
-            <h1 className="font-medium text-2xl">შეავსე ფორმა</h1>
+            <h1 className="text-2xl font-medium">შეავსე ფორმა</h1>
             <h6 className="text-sm text-[#000039]">შეავსე ფორმა სპეციალისტის გამოსაძახებლად</h6>
           </span>
         </div>
         <div className="flex flex-col items-center justify-center">
           <img src="https://i.imgur.com/XYU3U7L.png" alt="Service Image" />
           <span>
-            <h1 className="font-medium text-2xl">წავიყვანთ და შევამოწმებთ</h1>
+            <h1 className="text-2xl font-medium">წავიყვანთ და შევამოწმებთ</h1>
             <h6 className="text-sm text-[#000039]">სპეციალისტი წაიყვანს თქვენს ავტომობილს და შეამოწმებს</h6>
           </span>
         </div>
         <div className="flex flex-col items-center justify-center">
           <img src="https://i.imgur.com/uBHol7W.png" alt="Service Image" />
           <span>
-            <h1 className="font-medium text-2xl">შევაკეთებთ</h1>
+            <h1 className="text-2xl font-medium">შევაკეთებთ</h1>
             <h6 className="text-sm text-[#000039]">ჩვენ შეგითანხმებთ ყველა დეტალს, ფასს და შევაკეთებთ ავტომობილს</h6>
           </span>
         </div>
         <div className="flex flex-col items-center justify-center">
           <img src="https://i.imgur.com/9DiOqVS.png" alt="Service Image" />
           <span>
-            <h1 className="font-medium text-2xl">დაგიბრუნებთ</h1>
+            <h1 className="text-2xl font-medium">დაგიბრუნებთ</h1>
             <h6 className="text-sm text-[#000039]">შეკეთებულ ავტომობილს დაგიბრუნებთ სასურველ ლოკაციაზე.</h6>
           </span>
         </div>
       </div>
-      <div className="w-full grid grid-cols-2 text-center items-center mt-10 mb-14">
+      <div className="grid items-center w-full grid-cols-2 mt-10 text-center mb-14">
         <Link href="/contact" className="p-2 text-lg font-normal border border-solid border-[#FF7600] text-black">
           იხილეთ სიახლეები
         </Link>
@@ -65,4 +64,3 @@ export function MobileIndex() {
   );
 }
 
-MobileIndex.Layout = MobileLayout;
