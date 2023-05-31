c84fc03c3bb25b3c38f7f3f58554112c321174ba
Nika <arebanikko@gmail.com>
2023-05-31 08:25:27 +0400

    Layout Highly Improved

--- src/hooks/Layout.tsx
+++ src/hooks/Layout.tsx
@@ -2,15 +2,34 @@ import { PropsWithChildren } from "react";
 import { Navigation } from "@/components/desktopcomponents/h&f/navigation";
 import { Assistant } from "@/components/desktopcomponents/h&f/assistant";
 import { Footer } from "@/components/desktopcomponents/h&f/Footer";
+import { MobileTop } from "@/components/mobilecomponents/Menu/mobiletop";
+import { MobileBottom } from "@/components/mobilecomponents/Menu/mobilebottom";
 
 export function Layout({ children }: PropsWithChildren) {
   
   return (
     <div>
+
+<div className="md:hidden">
+<MobileTop />
+</div>
+
+<div className="hidden md:block">
       <Navigation />
+</div>
+  
       <main>{children}</main>
+  
+  <div className="hidden md:block">
       <Footer />
       <Assistant />
+  </div>
+
+
+      <div className="md:hidden">
+      <MobileBottom />
+      </div>
+
     </div>
   );
 }
